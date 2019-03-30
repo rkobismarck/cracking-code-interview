5815f4e97c4b2437220cb2430e678ea2dc523158
Roberto Trujillo <robertotrujillo@MacBook-Pro-de-Roberto-2.local>
2019-03-29 22:43:04 -0600

    StandardJS style corrections

--- is-unique/index.js
+++ is-unique/index.js
@@ -6,30 +6,29 @@
 		1.- Since the fact that we're using as a parameter a string, an input different from a string like undefined will be considered automatically like false.
 		2.- A string that should be evaluated at at least should have a length of two, because in the case of one it's evident the uniqueness condition, and an empty be considered as unique.
 */
-'use strict';
-const unique = (argument) =>{
-	
-	const isUnique = (argument) => {
-		// Basic input verification.
-		if (argument == undefined){
-			return false;
+'use strict'
+const unique = (argument) => {
+  const isUnique = (argument) => {
+    // Basic input verification.
+    if (argument === undefined) {
+      return false
 		}
-		if (argument.length > 1){
-			// Conversion to iterable object.
-			let elementCollection = argument.split("");
-			let mapUniqueness 	   = new Object();
-			for(let index = 0; index < elementCollection.length; index++){
-				let elementAtIndex = elementCollection[index]
-				if (mapUniqueness[elementAtIndex] != undefined){
-					return false;
+    if (argument.length > 1) {
+      // Conversion to iterable object.
+      let elementCollection = argument.split('')
+			let mapUniqueness = {}
+			for (let index = 0; index < elementCollection.length; index++) {
+        let elementAtIndex = elementCollection[index]
+        if (mapUniqueness[elementAtIndex] !== undefined) {
+          return false
 				}
-				mapUniqueness[elementAtIndex] = true;
+        mapUniqueness[elementAtIndex] = true
 			}
-		}
+    }
 
-		return true	
-	}
-	
-	return {isUnique}
+    return true
+  }
+
+  return { isUnique }
 }
-module.exports = unique();
+module.exports = unique()
--- is-unique/test.js
+++ is-unique/test.js
@@ -1,24 +1,25 @@
+/* global,it */
 'use strict'
 const app = require('./index.js')
 const assert = require('assert')
 
 describe('Validate the uniqueness of a string passed to the function.', () => {
   it('It should be able to return a false value, for a undefined input', () => {
-    assert.equal(false, app.isUnique())
+    assert.strictEqual(false, app.isUnique())
   })
   it('It should be able to return a true value, for an input with one character', () => {
-    assert.equal(true, app.isUnique('A'))
+    assert.strictEqual(true, app.isUnique('A'))
   })
   it('It should be able to return a true value, for an input with one character', () => {
-    assert.equal(true, app.isUnique(' '))
+    assert.strictEqual(true, app.isUnique(' '))
   })
   it('It should be able to return a false value, for an input with empty spaces', () => {
-    assert.equal(false, app.isUnique('  '))
+    assert.strictEqual(false, app.isUnique('  '))
   })
   it('It should be able to return a true value, for an input with empty spaces', () => {
-    assert.equal(true, app.isUnique(' aBcd'))
+    assert.strictEqual(true, app.isUnique(' aBcd'))
   })
   it('It should be able to return a true value, for an input with empty spaces', () => {
-    assert.equal(true, app.isUnique('aB cdefg'))
+    assert.strictEqual(true, app.isUnique('aB cdefg'))
   })
 })
